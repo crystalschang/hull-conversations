29ef7b47824c69c5f7c753d821dccfbad87b1d6f
Romain Dardour <romain@Air2.local>
2013-07-18 01:40:56 -0600

    More love

--- source/javascripts/widgets/subjects.js
+++ source/javascripts/widgets/subjects.js
@@ -1,32 +1,12 @@
 Hull.widget('subjects', {
-  // Specify template.
   templates: ['list'],
   
   subjects: [
-    {name: 'World peace', color: '#3B5998'},
-    {name: 'Superbowl', color: '#00A0D1'},
-    {name: 'Mac vs PC', color: '#C8232C'},
-    // {name: 'dropbox', color: '#3D9AE8'},
-    // {name: 'evernote', color: '#5BA525'},
+    {name: 'facebook', color: '#3B5998'},
+    {name: 'twitter', color: '#00A0D1'},
+    {name: 'pinterest', color: '#C8232C'},
     {name: 'Prism', color: '#CC181E'}
-    // {name: 'spotify', color: '#81B71A'}
   ],
-
-  initialize: function() {
-    this.sandbox.on('hull.conversation.pick', this.pickConvo,this);
-  },
-
-  pickConvo: function(e){
-
-    var col = $('[data-hull-id="'+e+'"]').parents('.cols');
-    var icon = col.parents('.topic');
-    this.chooseTopic(icon.data('hull-topic'));
-    // var topics = $('.topic');
-    // var inactive = topics.not(icon);
-    // inactive.removeClass('active').addClass('hidden');
-    // icon.removeClass('hidden').addClass('active');
-    col.addClass('selected');
-  },
 
   beforeRender: function(data) {
     data.subjects = this.subjects;
@@ -34,47 +14,19 @@ Hull.widget('subjects', {
   },
   
   chooseTopic: function(topic){
-    var $body = $('body');
-    var color = '';
-    if(topic){
-      this.subjects.forEach(function(s) {
-        if(s.name == topic) {
-          color = s.color;
-        }
-      })
-      // Change background color
-      $body.css({
-        backgroundColor: color
-      });
-      $('.topic').each(function() {
-        var t = $(this);
-        if(t.hasClass(topic)) {
-          t.addClass('active').removeClass('hidden');
-        }
-        else {
-          t.removeClass('active').addClass('hidden');
-        }
-      });
-    } else{
-      $body.css({
-        backgroundColor: ''
-      });
-      $('.topic').removeClass('hidden active')
-    }
 
   },
 
   actions: {
-    back: function(e,data){
-      this.chooseTopic('');
-    },
     select: function(e, data) {
-      
-      var id = '~'+btoa(data.data.uid);
-      // this.sandbox.emit('hull.conversation.reload', id);
-      // this.sandbox.emit('hull.conversation.reset_form', id);
-      this.chooseTopic(data.data.uid);
-
+      if(data.data.uid){
+        var id = '~'+btoa(data.data.uid);
+      } else {
+        var id=false;
+        //Todo Make this work
+      }
+      this.sandbox.emit('hull.conversation.reload', id);
+      this.sandbox.emit('hull.conversation.reset_form', id);
     }
   }
 });
